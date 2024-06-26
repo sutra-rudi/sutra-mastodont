9177919385dabb0f7bfc1b4b24c433a8d52a4304
sutra-rudi <rudi@sutra.hr>
2024-06-26 14:11:11 +0200

    seo about us

--- src/app/[lang]/about-us/page.tsx
+++ src/app/[lang]/about-us/page.tsx
@@ -2,6 +2,33 @@ import { getAllONamaQuery } from '@/app/queries/getAllONamaQuery';
 import PageContent from './PageContent';
 import { getSuffixFromLang } from '@/app/langUtils/getSuffixFromLang';
 
+export async function generateMetadata({ params: { lang } }: { params: { lang: string } }) {
+  const getAboutUs = await fetch(`${process.env.CMS_BASE_URL}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: getAllONamaQuery(lang),
+    }),
+    cache: 'no-cache',
+  });
+
+  const parseData = await getAboutUs.json();
+
+  const prepareDataForClient = parseData.data.allONama.edges[0].node;
+
+  const l = getSuffixFromLang(lang);
+
+  const seoField = prepareDataForClient[`seo${l}`];
+
+  return {
+    title: prepareDataForClient[`oNamaSadrzaj${l}`]?.[`tekstoviPodstraniceONama${l}`].oNamaNaslovHeroSekcija,
+    description: seoField[`seoTekst${l}`],
+    keywords: seoField[`seoTagovi${l}`].split(', ').map((keyw: string) => keyw),
+  };
+}
+
 export default async function AboutUsPage({ params: { lang } }: { params: { lang: string } }) {
   const getAboutUs = await fetch(`${process.env.CMS_BASE_URL}`, {
     method: 'POST',
@@ -19,6 +46,8 @@ export default async function AboutUsPage({ params: { lang } }: { params: { lang
   const prepareDataForClient = parseData.data.allONama.edges[0].node;
 
   const l = getSuffixFromLang(lang);
+
+  console.log('prep', prepareDataForClient);
 
   const pageContent = {
     title: prepareDataForClient[`oNamaSadrzaj${l}`]?.[`tekstoviPodstraniceONama${l}`],
