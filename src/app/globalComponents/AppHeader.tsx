8c262f1c03bb35750f2c8a5001842e61f802a42f
sutra-rudi <rudi@sutra.hr>
2024-07-08 10:44:08 +0200

    making navbar more flexible

--- src/app/globalComponents/AppHeader.tsx
+++ src/app/globalComponents/AppHeader.tsx
@@ -4,7 +4,9 @@ import Link from 'next/link';
 import { usePathname, useSearchParams } from 'next/navigation';
 import React from 'react';
 import { AppButtonGradient } from '../components/AppButton';
-
+import { Twirl as Hamburger } from 'hamburger-react';
+import { LuSun as SunIcon, LuMoon as MoonIcon } from 'react-icons/lu';
+import { Hr, Gb, It, De } from 'react-flags-select';
 const AppHeader = () => {
   const currentPath = usePathname();
   const searchParams = useSearchParams();
@@ -12,11 +14,13 @@ const AppHeader = () => {
   const splitPath = currentPath.split('/');
   const currentLang = splitPath[1];
 
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = React.useState<boolean>(false);
+
   const langs = [
-    { title: 'Hr', lang: 'hr' },
-    { title: 'Eng', lang: 'eng' },
-    { title: 'Ger', lang: 'ger' },
-    { title: 'Ita', lang: 'ita' },
+    { title: 'Hrvatski', lang: 'hr', flag: <Hr width={24} height={24} /> },
+    { title: 'English', lang: 'eng', flag: <Gb width={24} height={24} /> },
+    { title: 'Deutsch', lang: 'ger', flag: <De width={24} height={24} /> },
+    { title: 'Italiano', lang: 'ita', flag: <It width={24} height={24} /> },
   ];
 
   const navLinks = [
@@ -82,10 +86,10 @@ const AppHeader = () => {
   }, [theme]);
 
   return (
-    <nav className='bg-white dark:bg-gray-800 antialiased'>
+    <nav className='bg-white dark:bg-gray-800 antialiased relative'>
       <div className='max-w-screen-xl px-4 mx-auto 2xl:px-0 py-4'>
         <div className='flex items-center justify-between'>
-          <div className='flex items-center space-x-8'>
+          <div className='flex items-center'>
             <div className='shrink-0'>
               <a href={`/${currentLang}`} title=''>
                 <picture>
@@ -105,20 +109,28 @@ const AppHeader = () => {
               </a>
             </div>
 
-            <ul className='hidden lg:flex items-center justify-start gap-6 md:gap-8 py-3 sm:justify-center flex-wrap'>
-              {navLinks.map((navLink) => (
-                <li key={navLink.title} className='shrink-0'>
-                  <a
-                    href={navLink.url}
-                    className='flex text-sm font-medium text-gray-900 hover:text-primary-700 dark:text-white dark:hover:text-primary-500'
-                  >
-                    {navLink.title}
-                  </a>
-                </li>
-              ))}
-            </ul>
+            <div
+              className={`absolute w-full h-screen bg-red-300 inset-0 transition-all duration-300 flex items-center justify-center ${
+                isMobileMenuOpen
+                  ? 'opacity-100 pointer-events-auto select-auto'
+                  : 'opacity-0 select-none pointer-events-none'
+              }`}
+            >
+              <ul className='grid 2xl:grid-cols-3 lg:grid-cols-2 grid-cols-1 gap-x-4 gap-y-2'>
+                {navLinks.map((navLink) => (
+                  <li key={navLink.title} className='shrink-0'>
+                    <a
+                      href={navLink.url}
+                      className='flex text-h3 font-medium text-gray-900 hover:text-primary-700 dark:text-white dark:hover:text-primary-500'
+                    >
+                      {navLink.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
-          <div className='flex items-center space-x-4'>
+          <div className='flex items-center space-x-4 z-40'>
             {langs.map((language) => (
               <Link
                 key={language.lang}
@@ -128,13 +140,25 @@ const AppHeader = () => {
               >
                 <button
                   disabled={currentLang === language.lang}
-                  className='text-sm font-medium text-gray-900 dark:text-white'
+                  className='text-sm font-medium text-gray-900 dark:text-white flex place-items-center gap-2'
                 >
-                  {language.title}
+                  {language.flag} <span>{language.title.substring(0, 3)}</span>
                 </button>
               </Link>
             ))}
-            <AppButtonGradient action={handleTheme} buttonText='Theme switcher' />
+
+            <div
+              onClick={handleTheme}
+              className='z-40 cursor-pointer outline outline-1 rounded-full outline-offset-4 outline-almost-black dark:outline-almost-white transition-all duration-300 ease-linear'
+            >
+              {theme === 'light' ? <SunIcon size={24} color='#181816' /> : <MoonIcon size={24} color='#F8F7F2' />}
+            </div>
+            <div className='w-min z-40'>
+              <Hamburger
+                color={theme === 'light' ? '#181816' : '#F8F7F2'}
+                onToggle={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+              />
+            </div>
           </div>
         </div>
       </div>
