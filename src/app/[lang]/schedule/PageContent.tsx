35e32f4e2ceb56d0be7a7260b7493e4bc104667e
sutra-rudi <rudi@sutra.hr>
2024-07-25 11:48:32 +0200

    days done

--- src/app/[lang]/schedule/PageContent.tsx
+++ src/app/[lang]/schedule/PageContent.tsx
@@ -2,6 +2,7 @@
 
 import { getSuffixFromLang } from '@/app/langUtils/getSuffixFromLang';
 import React from 'react';
+import dayjs from 'dayjs';
 
 interface ScheduleInterface {
   content: any;
@@ -19,15 +20,33 @@ const PageContent = ({ content, lang }: ScheduleInterface) => {
     { hr: 'nedjelja', eng: 'Sunday', ger: 'Sonntag', ita: 'domenica' },
   ];
 
-  // console.log('PAGI CONT', content);
+  console.log('PAGI CONT', content);
 
   const l = getSuffixFromLang(lang);
   const contShorthand = content[`tjedniRaspored${l}`]?.[`tjedniRaspored${l}`];
+  const uvodShorthand = content.tjedniRasporedUvod;
+  const isSeasonal: boolean = uvodShorthand.sezonskoRadnoVrijeme;
+
+  //dayjs(contentCardShorthand.datum).format('DD.MM.YYYY') ?? 'Nema datuma'
 
   return (
     <article className='mx-auto my-0 max-w-[1024px] py-4'>
       <h2 className='w-full text-center text-7xl font-semibold pt-8 pb-8'>Raspored</h2>
 
+      {isSeasonal && (
+        <div className='py-4'>
+          <h4 className='font-medium text-accent text-lg'>Sezonsko radno vrijeme</h4>
+
+          <div className='w-full flex items-center justify-start'>
+            <p className='flex items-center justify-start gap-1'>
+              <span className='font-medium'>Vrijedi od:</span>
+              <span className='italic underline px-1'>{dayjs(uvodShorthand.vrijediOd).format('DD.MM.YYYY')}</span>
+              <span className='font-medium'>do:</span>
+              <span className='italic underline px-1'>{dayjs(uvodShorthand.vrijediDo).format('DD.MM.YYYY')}</span>
+            </p>
+          </div>
+        </div>
+      )}
       <div className='grid items-start gap-8 w-full'>
         <section className='relative overflow-x-auto'>
           <table className='w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400'>
