f2c7acf079cf782318d389feb485e51a25164f30
sutra-rudi <rudi@sutra.hr>
2024-08-12 12:04:53 +0200

    working on taxonomy

--- src/app/[lang]/blog/PageContent.tsx
+++ src/app/[lang]/blog/PageContent.tsx
@@ -6,12 +6,11 @@ import { getSuffixFromLang } from '@/app/langUtils/getSuffixFromLang';
 import { blogLanguageFields } from '@/app/pathsUtils/blogLanguageFields';
 import { slugifyOptions } from '@/app/pathsUtils/slugifyOptions';
 import dayjs from 'dayjs';
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import ReactPaginate from 'react-paginate';
 import { readingTime } from 'reading-time-estimator';
 import slugify from 'slugify';
 import { FaChevronLeft as PrevIcon, FaChevronRight as NextIcon } from 'react-icons/fa6';
-import Image from 'next/image';
 
 interface BlogArchivePage {
   pageContent: any[];
@@ -23,58 +22,65 @@ interface BlogArchivePage {
 
 const PageContent = ({ pageContent, totalPosts, adminSetup, lang, catList }: BlogArchivePage) => {
   const [currentPage, setCurrentPage] = useState(0);
-  const [filteredPosts, setFilteredPosts] = useState(pageContent);
   const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
   const [searchQuery, setSearchQuery] = useState('');
-  const [sortAlphabetically, setSortAlphabetically] = useState<string>(''); // State za abecedno sortiranje
-  const [sortByDate, setSortByDate] = useState<string>(''); // State za sortiranje po datumu
+  const [sortAlphabetically, setSortAlphabetically] = useState<string>('');
+  const [sortByDate, setSortByDate] = useState<string>('');
 
   const postsPerPage = Number(adminSetup.archiveItemsNumberOnSinglePage[0]);
   const offset = currentPage * postsPerPage;
 
   const l = getSuffixFromLang(lang);
 
-  // Sortiranje postova abecedno
-  const alphabeticallySortedPosts = useMemo(() => {
-    let postsToSort = [...filteredPosts];
-    if (sortAlphabetically === 'az') {
-      postsToSort.sort((a, b) => {
+  // Filtriranje i sortiranje postova
+  const processedPosts = useMemo(() => {
+    let posts = [...pageContent];
+
+    // Filtriranje prema pretrazi
+    if (adminSetup.activateSearchBar && searchQuery) {
+      posts = posts.filter((item: any) => {
+        const title = item.node[blogLanguageFields[lang]]?.[`naslovSadrzaj${l}`] ?? '';
+        const content = item.node[blogLanguageFields[lang]]?.[`sadrzajSadrzaj${l}`] ?? '';
+        return (
+          title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+          content.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+      });
+    }
+
+    // Filtriranje prema kategoriji
+    if (adminSetup.activateCategoryFilter && selectedCategory) {
+      posts = posts.filter((item: any) =>
+        item.node.introBlog.kategorija.edges.some(
+          (edge: any) => edge.node.informacijeKategorije[`imeKategorije${l}`] === selectedCategory
+        )
+      );
+    }
+
+    // Sortiranje postova
+    if (adminSetup.sortByAZ && sortAlphabetically === 'az') {
+      posts.sort((a, b) => {
         const titleA = a.node[blogLanguageFields[lang]]?.[`naslovSadrzaj${l}`]?.toLowerCase();
         const titleB = b.node[blogLanguageFields[lang]]?.[`naslovSadrzaj${l}`]?.toLowerCase();
         return titleA > titleB ? 1 : -1;
       });
     }
-    return postsToSort;
-  }, [sortAlphabetically, filteredPosts, lang, l]);
 
-  // Sortiranje postova po datumu
-  const dateSortedPosts = useMemo(() => {
-    let postsToSort = [...alphabeticallySortedPosts];
-    if (sortByDate === 'date') {
-      postsToSort.sort((a, b) => {
+    if (adminSetup.sortByDate && sortByDate === 'date') {
+      posts.sort((a, b) => {
         const dateA = dayjs(a.node.introBlog.datum);
         const dateB = dayjs(b.node.introBlog.datum);
         return dateB.diff(dateA);
       });
     }
-    return postsToSort;
-  }, [sortByDate, alphabeticallySortedPosts]);
 
-  // Filtriranje postova prema kategoriji i unosu pretraživanja
-  const filteredAndSearchedPosts = useMemo(() => {
-    return dateSortedPosts.filter((item) => {
-      const title = item.node[blogLanguageFields[lang]]?.[`naslovSadrzaj${l}`] ?? '';
-      const content = item.node[blogLanguageFields[lang]]?.[`sadrzajSadrzaj${l}`] ?? '';
-      return (
-        title.toLowerCase().includes(searchQuery.toLowerCase()) ||
-        content.toLowerCase().includes(searchQuery.toLowerCase())
-      );
-    });
-  }, [searchQuery, dateSortedPosts, lang, l]);
+    return posts;
+  }, [searchQuery, selectedCategory, sortAlphabetically, sortByDate, pageContent, lang, l, adminSetup]);
 
+  // Odabir trenutnih postova za prikaz na osnovu stranice
   const currentPosts = useMemo(
-    () => filteredAndSearchedPosts.slice(offset, offset + postsPerPage),
-    [offset, postsPerPage, filteredAndSearchedPosts]
+    () => processedPosts.slice(offset, offset + postsPerPage),
+    [offset, postsPerPage, processedPosts]
   );
 
   const handlePageClick = (event: { selected: number }) => {
@@ -83,16 +89,6 @@ const PageContent = ({ pageContent, totalPosts, adminSetup, lang, catList }: Blo
 
   const handleCategoryPick = (categoryName: string | null) => {
     setSelectedCategory(categoryName);
-    if (categoryName) {
-      const filtered = pageContent.filter((item: any) =>
-        item.node.introBlog.kategorija.edges.some(
-          (edge: any) => edge.node.informacijeKategorije[`imeKategorije${l}`] === categoryName
-        )
-      );
-      setFilteredPosts(filtered);
-    } else {
-      setFilteredPosts(pageContent);
-    }
     setCurrentPage(0);
   };
 
@@ -101,35 +97,38 @@ const PageContent = ({ pageContent, totalPosts, adminSetup, lang, catList }: Blo
 
     return (
       <div className='flex items-center justify-start gap-2 my-6'>
-        {catList.map((singleCat: any, index) => {
-          const catShorthand = singleCat.node.informacijeKategorije;
-          return (
-            catShorthand[categoryName] && (
-              <span
-                onClick={() => handleCategoryPick(catShorthand[categoryName])}
-                className={`border rounded-sm px-2 py-1 cursor-pointer transition-all ease-in-out ${
-                  selectedCategory === catShorthand[categoryName]
-                    ? 'border-accent bg-accent text-white'
-                    : 'border-accent/10 hover:border-accent hover:bg-accent/10'
-                }`}
-                key={index}
-              >
-                {catShorthand[categoryName]}
-              </span>
-            )
-          );
-        })}
+        {adminSetup.activateCategoryFilter &&
+          catList.map((singleCat: any, index) => {
+            const catShorthand = singleCat.node.informacijeKategorije;
+            return (
+              catShorthand[categoryName] && (
+                <span
+                  onClick={() => handleCategoryPick(catShorthand[categoryName])}
+                  className={`border rounded-sm px-2 py-1 cursor-pointer transition-all ease-in-out ${
+                    selectedCategory === catShorthand[categoryName]
+                      ? 'border-accent bg-accent text-white'
+                      : 'border-accent/10 hover:border-accent hover:bg-accent/10'
+                  }`}
+                  key={index}
+                >
+                  {catShorthand[categoryName]}
+                </span>
+              )
+            );
+          })}
 
-        <span
-          onClick={() => handleCategoryPick(null)}
-          className={`border rounded-sm px-2 py-1 cursor-pointer transition-all ease-in-out ${
-            !selectedCategory
-              ? 'border-accent bg-accent text-white'
-              : 'border-accent/10 hover:border-accent hover:bg-accent/10'
-          }`}
-        >
-          {'sve'}
-        </span>
+        {adminSetup.activateCategoryFilter && (
+          <span
+            onClick={() => handleCategoryPick(null)}
+            className={`border rounded-sm px-2 py-1 cursor-pointer transition-all ease-in-out ${
+              !selectedCategory
+                ? 'border-accent bg-accent text-white'
+                : 'border-accent/10 hover:border-accent hover:bg-accent/10'
+            }`}
+          >
+            {'sve'}
+          </span>
+        )}
       </div>
     );
   };
@@ -155,48 +154,53 @@ const PageContent = ({ pageContent, totalPosts, adminSetup, lang, catList }: Blo
       </div>
 
       {/* Polje za unos pretrage */}
-      <div className='max-w-[1440px] mx-auto my-4'>
-        <input
-          type='text'
-          value={searchQuery}
-          onChange={(e) => {
-            setSearchQuery(e.target.value);
-            setCurrentPage(0);
-          }}
-          placeholder='Pretraži blogove...'
-          className='w-full px-4 py-2 border border-accent/15 rounded max-w-lg transition-all ease-in-out focus:border-accent focus:ring-1 focus:ring-accent focus:outline-none placeholder:text-secondary-light active:placeholder:text-secondary-dark focus:placeholder:text-secondary-dark'
-        />
-      </div>
+      {adminSetup.activateSearchBar && (
+        <div className='max-w-[1440px] mx-auto my-4'>
+          <input
+            type='text'
+            value={searchQuery}
+            onChange={(e) => {
+              setSearchQuery(e.target.value);
+              setCurrentPage(0);
+            }}
+            placeholder='Pretraži blogove...'
+            className='w-full px-4 py-2 border border-accent/15 rounded max-w-lg transition-all ease-in-out focus:border-accent focus:ring-1 focus:ring-accent focus:outline-none placeholder:text-secondary-light active:placeholder:text-secondary-dark focus:placeholder:text-secondary-dark'
+          />
+        </div>
+      )}
 
-      {/* Polje za sortiranje abecedno */}
+      {/* Polje za sortiranje */}
       <div className='max-w-[1440px] mx-auto my-4 flex gap-4'>
-        <select
-          value={sortAlphabetically}
-          onChange={(e) => {
-            setSortAlphabetically(e.target.value);
-            setCurrentPage(0);
-          }}
-          className='px-4 py-2 border border-accent/15 rounded transition-all ease-in-out focus:border-accent focus:ring-1 focus:ring-accent focus:outline-none'
-        >
-          <option value=''>Sortiraj abecedno...</option>
-          <option value='az'>Abecedno (A-Z)</option>
-        </select>
+        {adminSetup.sortByAZ && (
+          <select
+            value={sortAlphabetically}
+            onChange={(e) => {
+              setSortAlphabetically(e.target.value);
+              setCurrentPage(0);
+            }}
+            className='px-4 py-2 border border-accent/15 rounded transition-all ease-in-out focus:border-accent focus:ring-1 focus:ring-accent focus:outline-none'
+          >
+            <option value=''>Sortiraj abecedno...</option>
+            <option value='az'>Abecedno (A-Z)</option>
+          </select>
+        )}
 
-        {/* Polje za sortiranje po datumu */}
-        <select
-          value={sortByDate}
-          onChange={(e) => {
-            setSortByDate(e.target.value);
-            setCurrentPage(0);
-          }}
-          className='px-4 py-2 border border-accent/15 rounded transition-all ease-in-out focus:border-accent focus:ring-1 focus:ring-accent focus:outline-none'
-        >
-          <option value=''>Sortiraj po datumu...</option>
-          <option value='date'>Po datumu</option>
-        </select>
+        {adminSetup.sortByDate && (
+          <select
+            value={sortByDate}
+            onChange={(e) => {
+              setSortByDate(e.target.value);
+              setCurrentPage(0);
+            }}
+            className='px-4 py-2 border border-accent/15 rounded transition-all ease-in-out focus:border-accent focus:ring-1 focus:ring-accent focus:outline-none'
+          >
+            <option value=''>Sortiraj po datumu...</option>
+            <option value='date'>Po datumu</option>
+          </select>
+        )}
       </div>
 
-      <div className='max-w-[1440px] mx-auto my-8 grid grid-cols-4 gap-4 '>
+      <div className='max-w-[1440px] mx-auto my-8 grid grid-cols-4 gap-4'>
         {currentPosts.map((blogContent: any, index: number) => {
           const contentShorthand = blogContent.node;
           const contentCardShorthand = contentShorthand.introBlog;
@@ -247,12 +251,12 @@ const PageContent = ({ pageContent, totalPosts, adminSetup, lang, catList }: Blo
         })}
       </div>
 
-      {filteredAndSearchedPosts.length > 0 && (
+      {processedPosts.length > 0 && (
         <ReactPaginate
           previousLabel={<PrevIcon className='transition-all ease-in-out group-hover:text-accent' />}
           nextLabel={<NextIcon className='transition-all ease-in-out group-hover:text-accent' />}
           breakLabel={'...'}
-          pageCount={Math.ceil(filteredAndSearchedPosts.length / postsPerPage)}
+          pageCount={Math.ceil(processedPosts.length / postsPerPage)}
           onPageChange={handlePageClick}
           forcePage={currentPage}
           containerClassName={'flex justify-center space-x-2 mt-6'}
