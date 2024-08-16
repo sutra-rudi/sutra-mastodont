0cfaf91d53b0dde18d56d012a8d1444b8dc71526
sutra-rudi <rudi@sutra.hr>
2024-08-16 11:33:15 +0200

    virt tours

---
+++ src/app/[lang]/360-tours/[tourId]/page.tsx
@@ -0,0 +1,3 @@
+export default async function SingleTourDisplay() {
+  return <main>ciaos</main>;
+}
---
+++ src/app/[lang]/360-tours/page.tsx
@@ -0,0 +1,3 @@
+export default async function VirtualTours() {
+  return <main>Ciaos bb</main>;
+}
