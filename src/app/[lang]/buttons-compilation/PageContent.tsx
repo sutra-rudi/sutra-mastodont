13cedb52062622a6635f11fc60ef21c6f95e8bba
sutra-rudi <rudi@sutra.hr>
2024-08-19 11:45:54 +0200

    working on buttons compilation

--- src/app/[lang]/buttons-compilation/PageContent.tsx
+++ src/app/[lang]/buttons-compilation/PageContent.tsx
@@ -9,11 +9,12 @@ const PageContent = () => {
       <h2 className='w-full text-h0_2xl font-medium text-center'>BOTUNI</h2>
 
       <div className='w-full flex items-center gap-4 flex-col'>
-        <h3 className='text-h3_md'>Bazni botun</h3>
+        <h3 className='text-h3_md'>Bazni botun + accent botun</h3>
         <div className='flex items-center justify-start gap-2'>
-          <SutraButtonBase innerText='Small Button' size='small' />
-          <SutraButtonBase innerText='Base Button' size='normal' />
-          <SutraButtonBase innerText='Large Button' size='large' />
+          <SutraButtonBase innerText='Small Button' size='small' isAccentButton={false} />
+          <SutraButtonBase innerText='Base Button' size='normal' isAccentButton={false} />
+          <SutraButtonBase innerText='Large Button' size='large' isAccentButton={false} />
+          <SutraButtonBase innerText='Accent Button' size='normal' isAccentButton={true} />
         </div>
       </div>
     </div>
--- src/app/components/SutraButton.tsx
+++ src/app/components/SutraButton.tsx
@@ -5,15 +5,20 @@ import React from 'react';
 interface SutraButtonBaseInterface {
   size: 'small' | 'normal' | 'large';
   innerText: string;
+  isAccentButton: boolean;
 }
 
-export const SutraButtonBase = ({ innerText, size }: SutraButtonBaseInterface) => {
+export const SutraButtonBase = ({ innerText, size, isAccentButton }: SutraButtonBaseInterface) => {
   return (
     <button
       role='button'
       className={`${
         size === 'small' ? 'text-sm' : size === 'normal' ? 'text-base' : size === 'large' ? 'text-lg' : ''
-      } transition-all ease-in-out py-[0.75rem] px-[1.125rem] bg-primary-dark text-sutraButtonText rounded-sutraButtonBorderRadius dark:bg-primary-light dark:text-primary-dark hover:bg-primary-light hover:text-primary-dark hover:scale-105 active:outline active:outline-sutraButtonOutline active:outline-accent dark:hover:bg-primary-dark dark:hover:text-secondary-light`}
+      } transition-all ease-in-out py-[0.75rem] px-[1.125rem] rounded-sutraButtonBorderRadius hover:scale-105 active:outline active:outline-sutraButtonOutline ${
+        isAccentButton
+          ? 'bg-accent text-sutraButtonText hover:text-accent hover:bg-primary-dark active:outline-accent'
+          : 'bg-primary-dark text-sutraButtonText  dark:bg-primary-light dark:text-primary-dark hover:bg-primary-light hover:text-primary-dark active:outline-accent dark:hover:bg-primary-dark dark:hover:text-secondary-light'
+      }  `}
     >
       {innerText}
     </button>
