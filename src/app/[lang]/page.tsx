4c1a63d032f78ba4f12515222e099a017f698e5c
sutra-rudi <rudi@sutra.hr>
2024-08-22 12:03:32 +0200

    deploy

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -117,7 +117,7 @@ export default async function Landing({ params: { lang } }: { params: { lang: st
       <Suspense>
         <main className='relative'>
           <HeroSection />
-          {blogDataArrayShorthand && (
+          {adminCtaSelection && blogDataArrayShorthand && (
             <BlogSection
               pageContent={blogDataArrayShorthand}
               lang={lang}
