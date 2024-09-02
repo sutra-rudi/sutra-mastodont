34485182f780d4bd2a6cc19b01d9ca6a307f0f38
sutra-rudi <rudi@sutra.hr>
2024-09-02 11:54:24 +0200

    inspectlet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -188,6 +188,29 @@ export default async function RootLayout({
             `}
             </Script>
           )}
+
+        {adminTokenDataShorthand.kodoviAdminApi.inspectlet && (
+          <Script id='inspectlet-script' strategy='afterInteractive'>
+            {`
+              window.__insp = window.__insp || [];
+              __insp.push(['wid', ${adminTokenDataShorthand.kodoviAdminApi.inspectlet}]);
+              (function() {
+                  function ldinsp() {
+                      if (typeof window.__inspld != "undefined") return;
+                      window.__inspld = 1;
+                      var insp = document.createElement('script');
+                      insp.type = 'text/javascript';
+                      insp.async = true;
+                      insp.id = "inspsync";
+                      insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js?wid=${adminTokenDataShorthand.kodoviAdminApi.inspectlet}&r=' + Math.floor(new Date().getTime()/3600000);
+                      var x = document.getElementsByTagName('script')[0];
+                      x.parentNode.insertBefore(insp, x);
+                  }
+                  setTimeout(ldinsp, 0);
+              })();
+            `}
+          </Script>
+        )}
       </body>
     </html>
   );
