7e170ecbf01d69fe21f893f6637b4700b55f7129
sutra-rudi <rudi@sutra.hr>
2024-09-02 13:38:41 +0200

    logosi

--- src/app/[lang]/PartnersSection.tsx
+++ src/app/[lang]/PartnersSection.tsx
@@ -2,14 +2,13 @@
 
 import Link from 'next/link';
 import React from 'react';
-import Marquee from 'react-marquee-slider';
+import Marquee from 'react-fast-marquee';
 
 interface PartnersSectionInterface {
   pageContent: any;
 }
 
 const PartnersSection = ({ pageContent }: PartnersSectionInterface) => {
-  // Dupliramo sadržaj kako bismo postigli neprekidno klizanje
   const duplicatedContent = [...pageContent, ...pageContent];
 
   return (
@@ -19,22 +18,36 @@ const PartnersSection = ({ pageContent }: PartnersSectionInterface) => {
         <h2 className='mb-8 lg:mb-16 text-3xl font-extrabold tracking-tight leading-tight text-center text-gray-900 dark:text-white md:text-4xl'>
           You&apos;ll be in good company
         </h2>
-        <div className='overflow-hidden'>
-          <Marquee
-            velocity={12}
-            scatterRandomly={false}
-            onFinish={() => null}
-            onInit={() => null}
-            resetAfterTries={200}
-            direction='ltr' // Lijevo na desno za kontinuirani efekt
-          >
+
+        <Marquee direction='left'>
+          {duplicatedContent.map((cont: any) => {
+            const imageSourcesShorthand = cont.node.logotipiPartnera.logoPNG.node;
+            return (
+              cont.node.logotipiPartnera.prikaziNaNaslovnici && (
+                <Link
+                  key={cont.node.id}
+                  className='block mx-4 flex-shrink-0'
+                  href={cont.node.logotipiPartnera.linkNaStranicuKlijentapartnera}
+                >
+                  <picture>
+                    <source srcSet={imageSourcesShorthand.srcSet} sizes={imageSourcesShorthand.sizes} />
+                    <img className='h-24 w-auto' src={imageSourcesShorthand.sourceUrl} alt='partner logo' />
+                  </picture>
+                </Link>
+              )
+            );
+          })}
+        </Marquee>
+
+        <div className='w-full flex items-center justify-start gap-6 bg-primary-dark dark:bg-primary-light mt-16 overflow-hidden'>
+          <Marquee direction='right'>
             {duplicatedContent.map((cont: any) => {
               const imageSourcesShorthand = cont.node.logotipiPartnera.logoPNG.node;
               return (
                 cont.node.logotipiPartnera.prikaziNaNaslovnici && (
                   <Link
                     key={cont.node.id}
-                    className='block mx-4'
+                    className='block mx-4 flex-shrink-0'
                     href={cont.node.logotipiPartnera.linkNaStranicuKlijentapartnera}
                   >
                     <picture>
@@ -46,6 +59,26 @@ const PartnersSection = ({ pageContent }: PartnersSectionInterface) => {
               );
             })}
           </Marquee>
+        </div>
+
+        <div className='grid grid-cols-3 items-center mt-16'>
+          {duplicatedContent.map((cont: any) => {
+            const imageSourcesShorthand = cont.node.logotipiPartnera.logoPNG.node;
+            return (
+              cont.node.logotipiPartnera.prikaziNaNaslovnici && (
+                <Link
+                  key={cont.node.id}
+                  className=' mx-4 flex-shrink-0 w-full px-12 py-10 border border-secondary-light flex items-center justify-center'
+                  href={cont.node.logotipiPartnera.linkNaStranicuKlijentapartnera}
+                >
+                  <picture>
+                    <source srcSet={imageSourcesShorthand.srcSet} sizes={imageSourcesShorthand.sizes} />
+                    <img className='h-24 w-auto' src={imageSourcesShorthand.sourceUrl} alt='partner logo' />
+                  </picture>
+                </Link>
+              )
+            );
+          })}
         </div>
       </div>
     </section>
