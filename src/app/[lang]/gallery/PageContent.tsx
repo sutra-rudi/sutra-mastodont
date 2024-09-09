e19e92d13af5d7628d858e3374408dda74befa53
sutra-rudi <rudi@sutra.hr>
2024-09-09 10:25:03 +0200

    gallery variants deploy

--- src/app/[lang]/gallery/PageContent.tsx
+++ src/app/[lang]/gallery/PageContent.tsx
@@ -181,7 +181,6 @@ const PageContent = () => {
             </h1>
             <p className='lg:pl-6 lg:w-2/3 mx-auto leading-relaxed text-base'>
               Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table.
-              Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom.
             </p>
           </div>
           <div className='flex flex-wrap md:-m-2 -m-1'>
