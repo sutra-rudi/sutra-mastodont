44d76b6bf69bba409a244cac9ae520df969346bd
sutra-rudi <rudi@sutra.hr>
2024-09-12 14:49:56 +0200

    kolacicic

--- src/app/components/CookiesNotification.tsx
+++ src/app/components/CookiesNotification.tsx
@@ -60,7 +60,7 @@ const CookieConsentNotification = ({ pageContent }: CookieInt) => {
 
   return (
     isVisible &&
-    userHasCookies && (
+    !userHasCookies && (
       <motion.div
         initial={{ opacity: 0, y: -20 }} // Početni stil
         animate={{ opacity: 1, y: 0 }} // Stil kad se animira
