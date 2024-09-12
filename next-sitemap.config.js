a24a5b3c12aef765ce182f68553ea89470fc4956
sutra-rudi <rudi@sutra.hr>
2024-09-12 12:21:05 +0200

    sitemap update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://sutra-mastodont.vercel.app/',
+  siteUrl: 'https://sutra-mastodont.vercel.app/hr/',
   exclude: ['/icon.svg', '/apple-icon.png', '/manifest.webmanifest', '/tags/*'],
   generateRobotsTxt: true,
   generateIndexSitemap: true,
