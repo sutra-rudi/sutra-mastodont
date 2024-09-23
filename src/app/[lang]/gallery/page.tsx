80c595e4d012cadc43a170b9912e9c351d80b6d8
sutra-rudi <rudi@sutra.hr>
2024-09-23 10:26:32 +0200

    deploy

--- src/app/[lang]/gallery/page.tsx
+++ src/app/[lang]/gallery/page.tsx
@@ -54,6 +54,8 @@ export default async function GalleryPage() {
       filterImages(media.galleryImages.gallery6),
     ]);
 
+  console.log('deploy');
+
   return (
     <main className='w-full min-h-screen bg-almost-white dark:bg-almost-black'>
       <LazyContent
