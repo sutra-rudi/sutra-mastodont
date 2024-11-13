99f33c478d4ef2f09c78bdc928befc54fc80b9ef
sutra-rudi <rudi@sutra.hr>
2024-11-13 14:24:03 +0100

    working on navbar versions

--- src/app/[lang]/nav-trake/PageContent.tsx
+++ src/app/[lang]/nav-trake/PageContent.tsx
@@ -4,14 +4,19 @@ import { PrimaryDarkButton } from '@/app/components/SutraButton';
 import Image from 'next/image';
 import React from 'react';
 import { FaArrowRight as RightIcon } from 'react-icons/fa6';
+import fbIcon from '../../imageMaterials/facebook-icon.svg';
+import instaIcon from '../../imageMaterials/instagram-icon.svg';
+import ytIcon from '../../imageMaterials/youtube-icon.svg';
+import { Twirl as Hamburger } from 'hamburger-react';
 
 interface NavbarResources {
   logoPaths: Record<string, string>;
+  iconPaths: Record<string, any>;
 }
 
 const StaticDemoLinks = ['O nama', 'Proizvodi', 'Kontakt', 'Usluge', 'Projekti'];
 
-const PageContent = ({ logoPaths }: NavbarResources) => {
+const PageContent = ({ logoPaths, iconPaths }: NavbarResources) => {
   return (
     <div>
       <h2>Prvi nav</h2>
@@ -21,22 +26,55 @@ const PageContent = ({ logoPaths }: NavbarResources) => {
           className='w-full flex items-center justify-between bg-primarna-tamna h-nad-navbar-xl -px--xl---m'
         >
           <div className='flex items-center justify-start -gap--xl---xl'>
-            <div className='flex items-center justify-start gap-4'>
-              <div className='w-4 h-4 relative'>{/* <Image src={fb_icon} alt='Facebook icon' fill /> */}</div>
+            <div className='md:flex hidden items-center justify-start gap-4'>
+              <div className='w-4 h-4 relative'>
+                <Image src={fbIcon} alt='Facebook icon' fill />
+              </div>
 
-              <div className='w-4 h-4 relative'>{/* <Image src={insta_icon} alt='Instagram icon' fill /> */}</div>
-              <div className='w-4 h-4 relative'>{/* <Image src={yt_icon} alt='Youtube icon' fill /> */}</div>
+              <div className='w-4 h-4 relative'>
+                <Image src={instaIcon} alt='Instagram icon' fill />
+              </div>
+              <div className='w-4 h-4 relative'>
+                <Image src={ytIcon} alt='Youtube icon' fill />
+              </div>
             </div>
 
             <div className='flex items-center gap-9'>
               <div className='flex items-center justify-start gap-3 text-primarna-svijetla'>
-                <div className=''></div>
-                <span>info@sutra.hr</span>
+                <Image
+                  alt='Message icon'
+                  src={iconPaths.nadNavbar.mailLight}
+                  width={14}
+                  height={14}
+                  className='block dark:hidden'
+                />
+                <Image
+                  alt='Message icon'
+                  src={iconPaths.nadNavbar.mailDark}
+                  width={14}
+                  height={14}
+                  className='dark:block hidden'
+                />
+
+                <span className='md:text-tag-tablet text-tag-mobile'>info@sutra.hr</span>
               </div>
 
               <div className='flex items-center justify-start gap-3 text-primarna-svijetla'>
-                <div className='w-4 h-4 bg-accent-boja'></div>
-                <span>01 3213 354</span>
+                <Image
+                  alt='Message icon'
+                  src={iconPaths.nadNavbar.phoneLight}
+                  width={14}
+                  height={14}
+                  className='block dark:hidden'
+                />
+                <Image
+                  alt='Message icon'
+                  src={iconPaths.nadNavbar.phoneDark}
+                  width={14}
+                  height={14}
+                  className='dark:block hidden'
+                />
+                <span className='md:text-tag-tablet text-tag-mobile'>01 3213 354</span>
               </div>
             </div>
           </div>
@@ -44,16 +82,50 @@ const PageContent = ({ logoPaths }: NavbarResources) => {
         <div title='glavni nav' className='w-full flex items-center justify-between  h-navbar-xl -px--xl---m'>
           <Image src={logoPaths.horizontalLight} alt='site logo' width={156} height={40} />
 
-          <div className='flex items-center -gap--xl---l'>
-            <div className='flex items-center gap-7'>
+          <div className='flex items-center xl:-gap--xl---l lg:-gap--desktop---l md:-gap--tablet---l -gap--mobile---l'>
+            <div className='lg:flex hidden items-center gap-7'>
               {StaticDemoLinks.map((lnk) => (
-                <span key={lnk}>{lnk}</span>
+                <span key={lnk} className='lg:text-menu-tabs-desktop md:text-menu-tabs-tablet text-menu-tabs-mobile'>
+                  {lnk}
+                </span>
               ))}
             </div>
-
-            <PrimaryDarkButton innerText='Button text' size='base' frontIcon={RightIcon} />
-
-            <div className='w-6 h-6 bg-accent-boja flex items-center justify-center'>LS</div>
+            <div className='md:block hidden'>
+              <PrimaryDarkButton innerText='Button text' size='base' frontIcon={RightIcon} />
+            </div>
+            <div className='flex items-center justify-start gap-1'>
+              <Image
+                alt='Globe icon'
+                src={iconPaths.langSwitcherLight}
+                width={14}
+                height={14}
+                className='block dark:hidden'
+              />
+              <Image
+                alt='Globe icon'
+                src={iconPaths.langSwitcherDark}
+                width={14}
+                height={14}
+                className='dark:block hidden'
+              />
+              <Image
+                alt='Arrow icon'
+                src={iconPaths.dropdownArrows.downPraznaDark}
+                width={14}
+                height={14}
+                className='dark:block hidden'
+              />
+              <Image
+                alt='Arrow icon'
+                src={iconPaths.dropdownArrows.downPraznaLight}
+                width={7}
+                height={7}
+                className='dark:hidden block'
+              />
+            </div>
+            <div className='lg:hidden block'>
+              <Hamburger />
+            </div>
           </div>
         </div>
       </nav>
