bed18b1276b8e11c90764ce687ac34d5f92141fc
sutra-rudi <rudi@sutra.hr>
2024-11-13 14:56:32 +0100

    working on navbar versions

--- src/app/[lang]/nav-trake/PageContent.tsx
+++ src/app/[lang]/nav-trake/PageContent.tsx
@@ -187,6 +187,70 @@ const PageContent = ({ logoPaths, iconPaths }: NavbarResources) => {
           </div>
         </div>
       </nav>
+
+      <h2 className='w-full text-center xl:text-h2-xl lg:text-h2-desktop md:text-h2-tablet text-h2-mobile font-semibold pt-8 text-primarna-tamna dark:text-primarna-svijetla my-4'>
+        Verzija tri
+      </h2>
+
+      <nav className='w-full bg-divider-lightmode'>
+        <div
+          title='glavni nav'
+          className='w-full flex lg:flex-col items-center justify-center -px--xl---m py-5 xl:-gap--xl---m lg:-gap--desktop---m md:-gap--tablet---m -gap--mobile---m'
+        >
+          <Image
+            src={logoPaths.horizontalLight}
+            alt='site logo'
+            width={156}
+            height={40}
+            className='max-h-10 object-cover object-center'
+          />
+
+          <div className='flex items-center xl:-gap--xl---l lg:-gap--desktop---l md:-gap--tablet---l -gap--mobile---l  lg:justify-between justify-end w-full'>
+            <div title='div za layout' className='lg:block hidden opacity-0 select-none pointer-events-none'></div>
+            <div className='lg:flex hidden items-center gap-7'>
+              {StaticDemoLinks.map((lnk) => (
+                <span key={lnk} className='lg:text-menu-tabs-desktop md:text-menu-tabs-tablet text-menu-tabs-mobile'>
+                  {lnk}
+                </span>
+              ))}
+            </div>
+
+            <div className='flex items-center justify-start gap-1'>
+              <Image
+                alt='Globe icon'
+                src={iconPaths.langSwitcherLight}
+                width={14}
+                height={14}
+                className='block dark:hidden'
+              />
+              <Image
+                alt='Globe icon'
+                src={iconPaths.langSwitcherDark}
+                width={14}
+                height={14}
+                className='dark:block hidden'
+              />
+              <Image
+                alt='Arrow icon'
+                src={iconPaths.dropdownArrows.downPraznaDark}
+                width={14}
+                height={14}
+                className='dark:block hidden'
+              />
+              <Image
+                alt='Arrow icon'
+                src={iconPaths.dropdownArrows.downPraznaLight}
+                width={7}
+                height={7}
+                className='dark:hidden block'
+              />
+            </div>
+            <div className='lg:hidden block'>
+              <Hamburger />
+            </div>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 };
