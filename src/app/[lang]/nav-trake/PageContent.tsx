993eeda7bd302d88af19b2b1a81fe972662f0fc5
sutra-rudi <rudi@sutra.hr>
2024-11-13 14:28:50 +0100

    working on navbar versions

--- src/app/[lang]/nav-trake/PageContent.tsx
+++ src/app/[lang]/nav-trake/PageContent.tsx
@@ -19,7 +19,9 @@ const StaticDemoLinks = ['O nama', 'Proizvodi', 'Kontakt', 'Usluge', 'Projekti']
 const PageContent = ({ logoPaths, iconPaths }: NavbarResources) => {
   return (
     <div>
-      <h2>Prvi nav</h2>
+      <h2 className='w-full text-center xl:text-h2-xl lg:text-h2-desktop md:text-h2-tablet text-h2-mobile font-semibold pt-8 text-primarna-tamna dark:text-primarna-svijetla my-4'>
+        Verzija jedan
+      </h2>
       <nav className='w-full bg-divider-lightmode'>
         <div
           title='nad nav'
@@ -79,6 +81,62 @@ const PageContent = ({ logoPaths, iconPaths }: NavbarResources) => {
             </div>
           </div>
         </div>
+        <div title='glavni nav' className='w-full flex items-center justify-between  h-navbar-xl -px--xl---m'>
+          <Image src={logoPaths.horizontalLight} alt='site logo' width={156} height={40} />
+
+          <div className='flex items-center xl:-gap--xl---l lg:-gap--desktop---l md:-gap--tablet---l -gap--mobile---l'>
+            <div className='lg:flex hidden items-center gap-7'>
+              {StaticDemoLinks.map((lnk) => (
+                <span key={lnk} className='lg:text-menu-tabs-desktop md:text-menu-tabs-tablet text-menu-tabs-mobile'>
+                  {lnk}
+                </span>
+              ))}
+            </div>
+            <div className='md:block hidden'>
+              <PrimaryDarkButton innerText='Button text' size='base' frontIcon={RightIcon} />
+            </div>
+            <div className='flex items-center justify-start gap-1'>
+              <Image
+                alt='Globe icon'
+                src={iconPaths.langSwitcherLight}
+                width={14}
+                height={14}
+                className='block dark:hidden'
+              />
+              <Image
+                alt='Globe icon'
+                src={iconPaths.langSwitcherDark}
+                width={14}
+                height={14}
+                className='dark:block hidden'
+              />
+              <Image
+                alt='Arrow icon'
+                src={iconPaths.dropdownArrows.downPraznaDark}
+                width={14}
+                height={14}
+                className='dark:block hidden'
+              />
+              <Image
+                alt='Arrow icon'
+                src={iconPaths.dropdownArrows.downPraznaLight}
+                width={7}
+                height={7}
+                className='dark:hidden block'
+              />
+            </div>
+            <div className='lg:hidden block'>
+              <Hamburger />
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <h2 className='w-full text-center xl:text-h2-xl lg:text-h2-desktop md:text-h2-tablet text-h2-mobile font-semibold pt-8 text-primarna-tamna dark:text-primarna-svijetla my-4'>
+        Verzija dva
+      </h2>
+
+      <nav className='w-full bg-divider-lightmode'>
         <div title='glavni nav' className='w-full flex items-center justify-between  h-navbar-xl -px--xl---m'>
           <Image src={logoPaths.horizontalLight} alt='site logo' width={156} height={40} />
 
