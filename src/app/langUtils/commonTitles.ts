fb9540a31e5096553fbbcccd12dc04ea8bb79ebe
sutra-rudi <rudi@sutra.hr>
2025-04-03 12:34:41 +0200

    deploy

--- src/app/langUtils/commonTitles.ts
+++ src/app/langUtils/commonTitles.ts
@@ -1,14 +1,14 @@
-import { UserLanguage } from '../enums/LangEnum';
+// import { UserLanguage } from '../enums/LangEnum';
 
-const commonTitles = {
-  [UserLanguage.eng]: 'Category',
-  [UserLanguage.hr]: 'Kategorija',
-  [UserLanguage.ita]: 'Categoria',
-  [UserLanguage.ger]: 'Kategorie',
-  // [UserLanguage.esp]: 'EspCat',
-  // [UserLanguage.fra]: 'FraCat',
-};
+// const commonTitles = {
+//   [UserLanguage.eng]: 'Category',
+//   [UserLanguage.hr]: 'Kategorija',
+//   [UserLanguage.ita]: 'Categoria',
+//   [UserLanguage.ger]: 'Kategorie',
+//   // [UserLanguage.esp]: 'EspCat',
+//   // [UserLanguage.fra]: 'FraCat',
+// };
 
-export const generateCommonTitles = (lang: UserLanguage): string => {
-  return commonTitles[lang] || '';
-};
+// export const generateCommonTitles = (lang: UserLanguage): string => {
+//   return commonTitles[lang] || '';
+// };
