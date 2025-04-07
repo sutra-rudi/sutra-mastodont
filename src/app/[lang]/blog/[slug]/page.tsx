918772c693cd729471716fb7c196a2b0511fcc01
sutra-rudi <rudi@sutra.hr>
2025-04-07 15:05:58 +0200

    landing page v2 update

--- src/app/[lang]/blog/[slug]/page.tsx
+++ src/app/[lang]/blog/[slug]/page.tsx
@@ -8,7 +8,11 @@ import dayjs from 'dayjs';
 import { fetchMediaPaths } from '@/app/utils/callMediaPaths';
 
 import updateLocale from 'dayjs/plugin/updateLocale';
-import ClientContent from './ClientContent';
+
+import dynamic from 'next/dynamic';
+import { Suspense } from 'react';
+
+const ClientContent = dynamic(() => import('./ClientContent'), { ssr: false });
 dayjs.extend(updateLocale);
 
 dayjs.updateLocale('en', {
@@ -152,53 +156,55 @@ export default async function SingleBlogPage({ params: { lang, slug } }: { param
   // console.log('BBB', bData.data.blog);
 
   return (
-    <main className='w-full xl:-pb--xl---5xl lg:-pb--desktop---5xl md:-pb--tablet---5xl -pb--mobile---5xl '>
-      <div className='bg-accent-boja/35 text-accent-boja block max-w-max rounded-[36px] text-sm uppercase py-1 px-3 lg:-mt--desktop---3xl md:-mt--tablet---5xl -mt--mobile---5xl mx-auto'>
-        {kategorija}
-      </div>
-      <p className='relative max-w-max flex items-center gap-2 text-accent-boja mx-auto lg:-mt--desktop---l md:-mt--tablet---l -mt--mobile---l px-4'>
-        <span>{`${author.node.firstName} ${author.node.lastName}`}</span>
-        <span className='block size-1 rounded-full bg-accent-boja'></span>
-        <span>{dayjs(datum).format('DD.MM.YYYY')}</span>
-      </p>
-      <h2 className='lg:text-h2-desktop md:text-h2-tablet text-h2-mobile max-w-max block text-center mx-auto relative px-4'>
-        {naslovBloga}
-      </h2>
+    <main className='w-full xl:-pb--xl---5xl lg:-pb--desktop---5xl md:-pb--tablet---5xl -pb--mobile---5xl min-h-screen'>
+      <Suspense>
+        <div className='bg-accent-boja/35 text-accent-boja block max-w-max rounded-[36px] text-sm uppercase py-1 px-3 lg:-mt--desktop---3xl md:-mt--tablet---5xl -mt--mobile---5xl mx-auto'>
+          {kategorija}
+        </div>
+        <p className='relative max-w-max flex items-center gap-2 text-accent-boja mx-auto lg:-mt--desktop---l md:-mt--tablet---l -mt--mobile---l px-4'>
+          <span>{`${author.node.firstName} ${author.node.lastName}`}</span>
+          <span className='block size-1 rounded-full bg-accent-boja'></span>
+          <span>{dayjs(datum).format('DD.MM.YYYY')}</span>
+        </p>
+        <h2 className='lg:text-h2-desktop md:text-h2-tablet text-h2-mobile max-w-max block text-center mx-auto relative px-4'>
+          {naslovBloga}
+        </h2>
 
-      <div className='text-ellipsis line-clamp-1 max-w-prose mx-auto text-center relative lg:-mt--desktop-h1-2---naslov-nadnaslov md:-mt--tablet-h1-2---naslov-nadnaslov -mt--mobile-h1-2---naslov-nadnaslov lg:text-nadnaslov-desktop md:text-nadnaslov-tablet text-nadnaslov-mobile px-4'>
-        {parse(introBloga)}
-      </div>
+        <div className='text-ellipsis line-clamp-1 max-w-prose mx-auto text-center relative lg:-mt--desktop-h1-2---naslov-nadnaslov md:-mt--tablet-h1-2---naslov-nadnaslov -mt--mobile-h1-2---naslov-nadnaslov lg:text-nadnaslov-desktop md:text-nadnaslov-tablet text-nadnaslov-mobile px-4'>
+          {parse(introBloga)}
+        </div>
 
-      <div className='flex items-center relative mx-auto lg:mt-10 md:mt-7 mt-4 max-w-max gap-4'>
-        <picture>
+        <div className='flex items-center relative mx-auto lg:mt-10 md:mt-7 mt-4 max-w-max gap-4'>
+          <picture>
+            <img
+              src={author.node.avatar.url}
+              alt='author-avatar'
+              width={64}
+              height={64}
+              className='object-cover object-center aspect-auto block rounded-full'
+            />
+          </picture>
+
+          <div className='lg:text-text-base-l-mobiletablet text-text-base-base-mobiletablet'>
+            <p className='font-bold'>{`${author.node.firstName} ${author.node.lastName}`}</p>
+            <p className='font-normal'>{dayjs(datum).format('D MMMM YYYY')}</p>
+          </div>
+        </div>
+        <picture className='block relative lg:mt-10 md:mt-7 mt-4'>
           <img
-            src={author.node.avatar.url}
-            alt='author-avatar'
-            width={64}
-            height={64}
-            className='object-cover object-center aspect-auto block rounded-full'
+            src={naslovna}
+            alt={`Blog image, source: ${naslovna}`}
+            width={1920}
+            height={800}
+            className='object-cover object-center block aspect-auto lg:h-[500px] md:h-[400px] h-[250px]'
           />
         </picture>
-
-        <div className='lg:text-text-base-l-mobiletablet text-text-base-base-mobiletablet'>
-          <p className='font-bold'>{`${author.node.firstName} ${author.node.lastName}`}</p>
-          <p className='font-normal'>{dayjs(datum).format('D MMMM YYYY')}</p>
+        <div className='lg:prose-lg prose max-w-[750px] w-full mx-auto lg:mt-xl-slika-sadrzaj md:mt-desktop-slika-sadrzaj mt-tablet-slika-sadrzaj px-4'>
+          {parse(sadrzajBloga)}
         </div>
-      </div>
-      <picture className='block relative lg:mt-10 md:mt-7 mt-4'>
-        <img
-          src={naslovna}
-          alt={`Blog image, source: ${naslovna}`}
-          width={1920}
-          height={800}
-          className='object-cover object-center block aspect-auto lg:h-[500px] md:h-[400px] h-[250px]'
-        />
-      </picture>
-      <div className='lg:prose-lg prose max-w-[750px] w-full mx-auto lg:mt-xl-slika-sadrzaj md:mt-desktop-slika-sadrzaj mt-tablet-slika-sadrzaj px-4'>
-        {parse(sadrzajBloga)}
-      </div>
 
-      <ClientContent gallery={galleryBlog} files={fileList} currentLang={lang} />
+        <ClientContent gallery={galleryBlog} files={fileList} currentLang={lang} />
+      </Suspense>
     </main>
   );
 }
--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -3,30 +3,43 @@ export const maxDuration = 60;
 import AppHero from '../appComponents/landing/AppHero';
 import BaseCaruselSection from '../appComponents/landing/BaseCaruselSection';
 import BlogSection from '../appComponents/landing/BlogSection';
+import ContactSection from '../appComponents/landing/ContactSection';
 import ContentSectionFirst from '../appComponents/landing/ContentSectionFirst';
-import ContentSectionSecond from '../appComponents/landing/ContentSectionSecond';
 import FaqSection from '../appComponents/landing/FaqSection';
 import MapSection from '../appComponents/landing/MapSection';
 import getAllBlogs from '../queries/dynamicQueries/getAllBlogs';
 import { fetchData } from '../utils/callApi';
 
+//
+import dataset from '../staticData/staticQueryData.json';
+
+const findKaruselDataBase = dataset.data.allSlikeGalerijaKarusel.edges.find(
+  (list) => list.node.title === 'Naslovnica – Karusel slika'
+);
+
+const findKaruselDataMiddle = dataset.data.allSlikeGalerijaKarusel.edges.find(
+  (list) => list.node.title === 'Fotogalerija – Obilazak jezera Mir i Klifova Telascice'
+);
+const filterImagesBase = Object.values(findKaruselDataBase?.node.photoGallery30pcs!).filter((val) => val);
+const filterImagesMiddle = Object.values(findKaruselDataMiddle?.node.photoGallery30pcs!).filter((val) => val);
+
+//
 export default async function Landing({ params: { lang } }: { params: { lang: string } }) {
   const getBlogs = await fetchData(getAllBlogs());
   const blogsData = !getBlogs.error ? getBlogs.data.allBlog?.edges : null;
-
-  // const filterBlogsForLanding = blogsData.filter(
-  //   (blogItem: any) =>
-  //     blogItem.node.introBlog.istaknutoNaNaslovnici === true && blogItem.node.introBlog.statusBloga === true
-  // );
 
   return (
     <main className='relative w-full dark:bg-primarna-tamna min-h-screen'>
       <AppHero currentLang={lang} />
       <ContentSectionFirst />
-      <BaseCaruselSection />
-      <ContentSectionSecond />
+      <BaseCaruselSection dataset={filterImagesBase} />
+
+      <ContentSectionFirst reverse />
 
       {blogsData && <BlogSection currentLang={lang} blogList={blogsData} />}
+
+      <BaseCaruselSection dataset={filterImagesMiddle} />
+      <ContactSection />
 
       <FaqSection currentLang={lang} />
       <MapSection />
