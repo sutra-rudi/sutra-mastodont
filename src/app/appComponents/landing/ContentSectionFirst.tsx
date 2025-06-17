4dbc3a5c55a4fdcef45938de5cba636ca6b16f0c
sutra-rudi <rudi@sutra.hr>
2025-06-17 13:24:54 +0200

    datagtm

--- src/app/appComponents/landing/ContentSectionFirst.tsx
+++ src/app/appComponents/landing/ContentSectionFirst.tsx
@@ -184,8 +184,35 @@ export default function ContentSectionFirst({
                 })}
               </ul>
             )}
-            <div className='lg:-mt--desktop-h1-2---sadrzaj-cta md:-mt--tablet-h1-2---sadrzaj-cta -mt--mobile-h1-2---sadrzaj-cta'>
-              <PuniTamni />
+            <div
+              className='lg:-mt--desktop-h1-2---sadrzaj-cta md:-mt--tablet-h1-2---sadrzaj-cta -mt--mobile-h1-2---sadrzaj-cta'
+              // data-gtm='kornati-click'
+            >
+              {/* <PuniTamni /> */}
+
+              <button
+                data-gtm='kornati-click'
+                className='w-max max-w-[180px] lg:-mt--desktop-h1-2---sadrzaj-cta md:-mt--tablet-h1-2---sadrzaj-cta -mt--mobile-h1-2---sadrzaj-cta  flex items-center justify-start bg-primarna-tamna text-almost-white lg:text-button-desktop md:text-button-tablet text-button-mobile md:px-7 md:py-3 px-6 py-[15px] md:rounded-[7px] rounded-[5px] md:gap-[13.3px] gap-[11.6px] transition-all ease-in-out duration-300 group hover:bg-sekundarna-tamna active:bg-almost-black'
+              >
+                <span className='motion-ease-spring-bouncy group-hover:motion-preset-slide-up'>
+                  {/* {txt ? txt : 'Button text'} */}
+                  Button text
+                </span>
+
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  width='10'
+                  height='12'
+                  viewBox='0 0 10 12'
+                  fill='none'
+                  className='transition-all ease-in-out group-hover:translate-x-1'
+                >
+                  <path
+                    d='M0.333336 0.747747L0.333335 11.248C0.333685 11.3544 0.364457 11.4586 0.422337 11.5494C0.480216 11.6403 0.563013 11.7145 0.661814 11.7638C0.760617 11.8132 0.871683 11.836 0.983057 11.8297C1.09443 11.8234 1.20189 11.7883 1.29388 11.7281L9.25252 6.47799C9.58249 6.26041 9.58249 5.73656 9.25252 5.51838L1.29388 0.268233C1.20209 0.207463 1.09457 0.171825 0.983008 0.165193C0.871449 0.158561 0.760117 0.181188 0.661105 0.230615C0.562094 0.280042 0.479191 0.35438 0.421405 0.445551C0.36362 0.536721 0.33316 0.641239 0.333336 0.747747Z'
+                    fill='currentColor'
+                  />
+                </svg>
+              </button>
             </div>
           </div>
         </div>
