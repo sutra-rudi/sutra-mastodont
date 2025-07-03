fc3dccd28029a8fe40c5cd19043394f2006104b7
sutra-rudi <rudi@sutra.hr>
2025-07-03 11:02:21 +0200

    let's try

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,14 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   darkMode: 'class',
-  content: ['./src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+  mode: 'jit',
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './appComponents/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {
       // # Screens
